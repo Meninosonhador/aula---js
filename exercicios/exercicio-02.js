8cb9fe2e0c593f08de7fd9c14e07869aad1a822a
Guilherme Massareli <alcwesley877@gmail.com>
2020-01-16 22:25:23 -0300

    Subindo exercicios

---
+++ exercicios/exercicio-02.js
@@ -0,0 +1,8 @@
+let F
+let Celsius = prompt("Informe a temperatura")
+console.log("conteúdo inicíal")
+Celsius = parseFloat(Celsius)
+F = 1.8 * Celsius + 32
+console.log("A temperatura em fahrenheit é " + F)
+
+
---
+++ exercicios/exercicio-03.js
@@ -0,0 +1,5 @@
+let Sucessor = prompt("informe um número");
+let Antecessor = 18;
+
+let antecessor = prompt("informe outro número");
+console.log("O antecessor do número 19 é 18 e do sucessor é " + Sucessor);
