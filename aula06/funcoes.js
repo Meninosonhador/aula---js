cc1700c99aa8cd99bfff8850c7972c50edbf2d50
Guilherme Massareli <alcwesley877@gmail.com>
2020-02-03 22:20:46 -0300

    Formulários, média simples, trocar o nome pelo sobrenome

---
+++ aula06/funcoes.js
@@ -0,0 +1,6 @@
+
+function MudarSite(){
+    document.getElementById("titulo").innerHTML = "Meu site";
+
+    document.getElementById("login").innerHTML += "<a href='https://uol.com.br</a>"; // operador += mantem o que existia e adiciona o que vc digitou
+}
